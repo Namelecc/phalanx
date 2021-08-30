dda26da58bb5e0c8b2536c42192b60b886b5e363
Namelecc <39218601+Namelecc@users.noreply.github.com>
2021-08-29 22:16:10 -0400

    Update cheat_detected_games.py

--- Game-Stats/cheat_detected_games.py
+++ Game-Stats/cheat_detected_games.py
@@ -1,15 +1,18 @@
 import berserk
 user = "USERNAME"
-token = "API TOKEN"
+token = "TOKEN"
 
 session = berserk.TokenSession(token)
 client = berserk.Client(session=session)
 
-games = client.games.export_by_player(user)
+games = client.games.export_by_player(user, max = 10981)
 for game in games:
     if game['status'] == "cheat":
-            player_color = "black" 
-            if game['players']['white']['user']['id'] == user.casefold: 
-                player_color = "white"
-            if game['winner'] != player_color:
-                print(f"Cheater, {game['id']}")
+        
+        if game['players']['white']['user']['id'] == user.casefold():
+            player_color = "white"
+        else:
+            player_color = "black"
+                
+        if game['winner'] != player_color:
+            print(game['id'])
